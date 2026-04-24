f1f47a8c17c7efa04334304c679ce6ac2d4584a3
andkcode <akiricuk37@gmail.com>
2026-04-24 14:15:01 +0200

    not necessary page

--- src/App.tsx
+++ src/App.tsx
@@ -126,7 +126,7 @@ export default function App() {
 				<AboutGallery />
 				<Services />
 				<Certificates />
-				<About />
+				{/* <About /> */}
 				<Stages />
 				<Methods />
 				<DetoxAccordion />
